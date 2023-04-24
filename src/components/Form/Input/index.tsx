50d9107b2d4198f100d1f27e903d5d30caf2a2c8
Enrique Vierne <viernee@gmail.com>
2023-04-24 06:43:01 -0300

    feat: testing...

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,13 +1,13 @@
-import { StyledInputContainer } from '../../../styles/form';
-import { StyledParagraph } from '../../../styles/typography';
+import { StyledInputContainer } from "../../../styles/form";
+import { StyledParagraph } from "../../../styles/typography";
 
 const Input = ({ id }) => (
   <div>
     <StyledInputContainer>
-      <input type='text' id={id} placeholder=' ' />
+      <input type="text" id={id} placeholder=" " />
       <label htmlFor={id}>Teste</label>
     </StyledInputContainer>
-    <StyledParagraph fontColor='red'>Erro</StyledParagraph>
+    <StyledParagraph fontColor="red">Erro</StyledParagraph>
   </div>
 );
 
